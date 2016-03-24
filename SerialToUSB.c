6b83e07e1cd9ff3da36be088ca26c3ca43df5434
rgm <rgm3@users.noreply.github.com>
2016-03-24 12:04:33 -0500

    Reformat whitespace and cleanup

--- SerialToUSB.c
+++ SerialToUSB.c
@@ -1,5 +1,6 @@
 /* 
- * Serial to USB HID
+ * Serial mouse to USB HID using LUFA.
+ * Cribbed heavily from LUFA USBtoSerial and ClassDriver/Mouse demos.
  */
 
 #include "SerialToUSB.h"
@@ -11,51 +12,49 @@ static uint8_t PrevMouseHIDReportBuffer[sizeof(USB_MouseReport_Data_t)];
 static RingBuffer_t USARTtoUSB_Buffer;
 
 /** Underlying data buffer for \ref USARTtoUSB_Buffer, where the stored bytes are located. */
-static uint8_t      USARTtoUSB_Buffer_Data[128];
+static uint8_t      USARTtoUSB_Buffer_Data[64];
 
 /** LUFA HID Class driver interface configuration and state information. This structure is
  *  passed to all HID Class driver functions, so that multiple instances of the same class
  *  within a device can be differentiated from one another.
  */
 USB_ClassInfo_HID_Device_t Mouse_HID_Interface =
-	{
-		.Config =
-			{
-				.InterfaceNumber              = INTERFACE_ID_Mouse,
-				.ReportINEndpoint             =
-					{
-						.Address              = MOUSE_EPADDR,
-						.Size                 = MOUSE_EPSIZE,
-						.Banks                = 1,
-					},
-				.PrevReportINBuffer           = PrevMouseHIDReportBuffer,
-				.PrevReportINBufferSize       = sizeof(PrevMouseHIDReportBuffer),
-			},
-	};
+{
+    .Config =
+    {
+        .InterfaceNumber          = INTERFACE_ID_Mouse,
+        .ReportINEndpoint         =
+        {
+            .Address              = MOUSE_EPADDR,
+            .Size                 = MOUSE_EPSIZE,
+            .Banks                = 1,
+        },
+        .PrevReportINBuffer       = PrevMouseHIDReportBuffer,
+        .PrevReportINBufferSize   = sizeof(PrevMouseHIDReportBuffer),
+    },
+};
 
-
-
-#define LED 6 // PE6
 
 #define USART_BAUDRATE 1200
 #define BAUD_PRESCALE (((( F_CPU / 16) + ( USART_BAUDRATE / 2) ) / ( USART_BAUDRATE )) - 1)
 
-volatile char receivedByte = 0;
-
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
 int main(void)
 {
-  SetupHardware();
-  RingBuffer_InitBuffer(&USARTtoUSB_Buffer, USARTtoUSB_Buffer_Data, sizeof(USARTtoUSB_Buffer_Data));
+    SetupHardware();
+    RingBuffer_InitBuffer(&USARTtoUSB_Buffer, USARTtoUSB_Buffer_Data, sizeof(USARTtoUSB_Buffer_Data));
 
-  LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
-  GlobalInterruptEnable();
-  
-  for (;;) {
-    HID_Device_USBTask(&Mouse_HID_Interface);
-    USB_USBTask();
-  }
+    LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+    GlobalInterruptEnable();
 
-  return 0;
+    for (;;) {
+        HID_Device_USBTask(&Mouse_HID_Interface);
+        USB_USBTask();
+    }
+
+    return 0;
 }
 
 /** ISR to manage the reception of data from the serial port, placing received bytes into a circular buffer
@@ -63,81 +62,105 @@ int main(void)
  */
 ISR(USART1_RX_vect, ISR_BLOCK)
 {
-        uint8_t ReceivedByte = UDR1;
+    uint8_t ReceivedByte = UDR1;
 
-        if ((USB_DeviceState == DEVICE_STATE_Configured) && !(RingBuffer_IsFull(&USARTtoUSB_Buffer)))
-          RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
+    if ((USB_DeviceState == DEVICE_STATE_Configured) && !(RingBuffer_IsFull(&USARTtoUSB_Buffer)))
+        RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
 }
 
 /** Configures the board hardware and chip peripherals for the demo's functionality. */
 void SetupHardware(void)
 {
 #if (ARCH == ARCH_AVR8)
-	/* Disable watchdog if enabled by bootloader/fuses */
-	MCUSR &= ~(1 << WDRF);
-	wdt_disable();
+    /* Disable watchdog if enabled by bootloader/fuses */
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
 
-	/* Disable clock division */
-	clock_prescale_set(clock_div_1);
+    /* Disable clock division */
+    clock_prescale_set(clock_div_1);
 #elif (ARCH == ARCH_XMEGA)
-	/* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
-	XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
-	XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
+    /* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
+    XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
+    XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
 
-	/* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
-	XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
-	XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
+    /* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
+    XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
+    XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
 
-	PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
+    PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
 #endif
 
-  UCSR1B = (1 << RXEN1) | (1 << TXEN1); // Turn on the transmission and reception circuitry
-  UCSR1C = (1 << UCSZ11) | (0 << UCSZ10); // Use 7-bit character sizes
-  UBRR1H = (BAUD_PRESCALE >> 8);   // upper 8 bits of baud
-  UBRR1L = (BAUD_PRESCALE & 0xff); // lower 8 bits
+    MySerial_Init(USART_BAUDRATE);
 
-  UCSR1B |= (1 << RXCIE1 ); // enable receive complete interrupt
+    /* Hardware Initialization */
+    LEDs_Init();
+    Buttons_Init();
+    USB_Init();
+}
 
-	/* Hardware Initialization */
-	LEDs_Init();
-	Buttons_Init();
-	USB_Init();
+/** Initializes the USART, ready for serial data transmission and reception. This initializes the interface to
+ *  7-bit, no parity, 1 stop bit settings suitable for Microsoft mouse protocol use.
+ *
+ *  \param[in] BaudRate     Serial baud rate, in bits per second.
+ */
+void MySerial_Init(const uint32_t BaudRate) {
+    UCSR1B = (1 << RXEN1) | (1 << TXEN1); // Turn on the transmission and reception circuitry
+    UCSR1C = (1 << UCSZ11) | (0 << UCSZ10); // Use 7-bit character sizes
+    UBRR1H = (BAUD_PRESCALE >> 8);   // upper 8 bits of baud
+    UBRR1L = (BAUD_PRESCALE & 0xff); // lower 8 bits
+
+    UCSR1B |= (1 << RXCIE1 ); // enable receive complete interrupt
+
+    /* Start the flush timer so that overflows occur rapidly to push received bytes to the USB interface */
+    // TODO rgm: Read the datasheet and learn about this
+    //TCCR0B = (1 << CS02);
+    //UCSR1B = ((1 << RXCIE1) | (1 << TXEN1) | (1 << RXEN1));
+
+
+    /* LUFA wants to do this in its Serial_Init, investigate differences.
+	UBRR1  = SERIAL_UBBRVAL(BaudRate);
+	UCSR1C = ((1 << UCSZ11) | (1 << UCSZ10));
+	UCSR1A = (DoubleSpeed ? (1 << U2X1) : 0);
+	UCSR1B = ((1 << TXEN1)  | (1 << RXEN1));
+	DDRD  |= (1 << 3);
+	PORTD |= (1 << 2);
+    */
 }
 
 /** Event handler for the library USB Connection event. */
 void EVENT_USB_Device_Connect(void)
 {
-	LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+    LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
 }
 
 /** Event handler for the library USB Disconnection event. */
 void EVENT_USB_Device_Disconnect(void)
 {
-	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+    LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
 }
 
 /** Event handler for the library USB Configuration Changed event. */
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
-	bool ConfigSuccess = true;
+    bool ConfigSuccess = true;
 
-	ConfigSuccess &= HID_Device_ConfigureEndpoints(&Mouse_HID_Interface);
+    ConfigSuccess &= HID_Device_ConfigureEndpoints(&Mouse_HID_Interface);
 
-	USB_Device_EnableSOFEvents();
+    USB_Device_EnableSOFEvents();
 
-	LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
+    LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
 }
 
 /** Event handler for the library USB Control Request reception event. */
 void EVENT_USB_Device_ControlRequest(void)
 {
-	HID_Device_ProcessControlRequest(&Mouse_HID_Interface);
+    HID_Device_ProcessControlRequest(&Mouse_HID_Interface);
 }
 
 /** Event handler for the USB device Start Of Frame event. */
 void EVENT_USB_Device_StartOfFrame(void)
 {
-	HID_Device_MillisecondElapsed(&Mouse_HID_Interface);
+    HID_Device_MillisecondElapsed(&Mouse_HID_Interface);
 }
 
 /** HID class driver callback function for the creation of HID reports to the host.
@@ -151,43 +174,44 @@ void EVENT_USB_Device_StartOfFrame(void)
  *  \return Boolean \c true to force the sending of the report, \c false to let the library determine if it needs to be sent
  */
 bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-                                         uint8_t* const ReportID,
-                                         const uint8_t ReportType,
-                                         void* ReportData,
-                                         uint16_t* const ReportSize)
+        uint8_t* const ReportID,
+        const uint8_t ReportType,
+        void* ReportData,
+        uint16_t* const ReportSize)
 {
-      USB_MouseReport_Data_t* MouseReport = (USB_MouseReport_Data_t*)ReportData;
+    USB_MouseReport_Data_t* MouseReport = (USB_MouseReport_Data_t*)ReportData;
 
-      char button = 0;
-      int x = 0;
-      int y = 0; 
+    char button = 0;
+    int x = 0;
+    int y = 0; 
 
-      uint16_t BufferCount = RingBuffer_GetCount(&USARTtoUSB_Buffer);
-      if (BufferCount > 2)
-      {
+    // Read the 3-byte mouse data packet from the serial buffer
+    uint16_t BufferCount = RingBuffer_GetCount(&USARTtoUSB_Buffer);
+    if (BufferCount >= 3)
+    {
         unsigned char s[3];
-        while (BufferCount--)
+        for (int i = 0; i <=2; i++)
         {
-          s[2 - BufferCount] = RingBuffer_Remove(&USARTtoUSB_Buffer);
+            s[i] = RingBuffer_Remove(&USARTtoUSB_Buffer);
         }
 
         DecodeMouse(s, &button, &x, &y); 
     }
 
     if (x > 0)
-     MouseReport->X = 1;
+        MouseReport->X = 1;
     if (x < 0)
-     MouseReport->X = -1;
+        MouseReport->X = -1;
     if (y > 0)
-     MouseReport->Y = 1;
+        MouseReport->Y = 1;
     if (y < 0)
-     MouseReport->Y = -1;
+        MouseReport->Y = -1;
 
     if (button == 'l')
-     MouseReport->Button |= (1 << 0);
+        MouseReport->Button |= (1 << 0);
 
     if (button == 'r')
-     MouseReport->Button |= (1 << 1);
+        MouseReport->Button |= (1 << 1);
 
     *ReportSize = sizeof(USB_MouseReport_Data_t);
     return true;
@@ -202,28 +226,39 @@ bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDIn
  *  \param[in] ReportSize  Size in bytes of the received HID report
  */
 void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-                                          const uint8_t ReportID,
-                                          const uint8_t ReportType,
-                                          const void* ReportData,
-                                          const uint16_t ReportSize)
+        const uint8_t ReportID,
+        const uint8_t ReportType,
+        const void* ReportData,
+        const uint16_t ReportSize)
 {
-	// Unused (but mandatory for the HID class driver) in this demo, since there are no Host->Device reports
+    // Unused (but mandatory for the HID class driver) in this demo, since there are no Host->Device reports
 }
 
-/*
-   s should consist of 3 bytes from the mouse
-*/
+/** Microsoft Serial Mouse decoder.
+ *
+ * Thanks to Paul Bourke.
+ * http://paulbourke.net/dataformats/serialmouse/
+ *
+ * Every time the mouse changes state (moved or button pressed) a three byte "packet" is sent to the serial interface.
+ *
+ * \param[in]   s Pointer to 3 byte character array of serial data received from the mouse
+ * \param[out]  button  Pointer to a character representing button state - 'l' for left, 'r' for right
+ * \param[out]  x       Pointer to the X displacement integer, from -127 to +127
+ * \param[out]  y       Pointer to the Y displacement integer, from -127 to +127
+ */
 void DecodeMouse(unsigned char *s, char *button, int *x, int *y)
 {
-   *button = 'n'; /* No button - should only happen on an error */
-   if ((s[0] & 0x20) != 0)
-      *button = 'l';
-   else if ((s[0] & 0x10) != 0)
-      *button = 'r';
-   *x = (s[0] & 0x03) * 64 + (s[1] & 0x3F);
-   if (*x > 127)
-      *x = *x - 256;
-   *y = (s[0] & 0x0C) * 16 + (s[2] & 0x3F);
-   if (*y > 127)
-      *y = *y - 256;
+    *button = 'n'; /* No button - should only happen on an error */
+    if ((s[0] & 0x20) != 0)
+        *button = 'l';
+    else if ((s[0] & 0x10) != 0)
+        *button = 'r';
+    *x = (s[0] & 0x03) * 64 + (s[1] & 0x3F);
+    if (*x > 127)
+        *x = *x - 256;
+    *y = (s[0] & 0x0C) * 16 + (s[2] & 0x3F);
+    if (*y > 127)
+        *y = *y - 256;
 }
+
+// vim: noai:ts=4:sw=4
--- SerialToUSB.h
+++ SerialToUSB.h
@@ -1,32 +1,32 @@
 /*
-             LUFA Library
-     Copyright (C) Dean Camera, 2015.
+   LUFA Library
+   Copyright (C) Dean Camera, 2015.
 
-  dean [at] fourwalledcubicle [dot] com
-           www.lufa-lib.org
-*/
+   dean [at] fourwalledcubicle [dot] com
+   www.lufa-lib.org
+   */
 
 /*
-  Copyright 2015  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+   Copyright 2015  Dean Camera (dean [at] fourwalledcubicle [dot] com)
 
-  Permission to use, copy, modify, distribute, and sell this
-  software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in
-  all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
-  software without specific, written prior permission.
+   Permission to use, copy, modify, distribute, and sell this
+   software and its documentation for any purpose is hereby granted
+   without fee, provided that the above copyright notice appear in
+   all copies and that both that the copyright notice and this
+   permission notice and warranty disclaimer appear in supporting
+   documentation, and that the name of the author not be used in
+   advertising or publicity pertaining to distribution of the
+   software without specific, written prior permission.
 
-  The author disclaims all warranties with regard to this
-  software, including all implied warranties of merchantability
-  and fitness.  In no event shall the author be liable for any
-  special, indirect or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether
-  in an action of contract, negligence or other tortious action,
-  arising out of or in connection with the use or performance of
-  this software.
-*/
+   The author disclaims all warranties with regard to this
+   software, including all implied warranties of merchantability
+   and fitness.  In no event shall the author be liable for any
+   special, indirect or consequential damages or any damages
+   whatsoever resulting from loss of use, data or profits, whether
+   in an action of contract, negligence or other tortious action,
+   arising out of or in connection with the use or performance of
+   this software.
+   */
 
 /** \file
  *
@@ -36,64 +36,65 @@
 #ifndef _SERIAL_USB_H_
 #define _SERIAL_USB_H_
 
-		#define set_bit(address,bit) (address |= (1<<bit))
-		#define clear_bit(address,bit) (address &= ~(1<<bit))
-		#define toggle_bit(address,bit) (address ^= (1<<bit))
-		#define check_bit(address,bit) ((address & (1<<bit)) == (1<<bit))
+#define set_bit(address,bit) (address |= (1<<bit))
+#define clear_bit(address,bit) (address &= ~(1<<bit))
+#define toggle_bit(address,bit) (address ^= (1<<bit))
+#define check_bit(address,bit) ((address & (1<<bit)) == (1<<bit))
 
-	/* Includes: */
-		#include <avr/io.h>
-		#include <avr/wdt.h>
-		#include <avr/interrupt.h>
-		#include <avr/power.h>
-		#include <stdbool.h>
-		#include <string.h>
+/* Includes: */
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <stdbool.h>
+#include <string.h>
 
-		#include "Descriptors.h"
+#include "Descriptors.h"
 
-		#include <LUFA/Drivers/Board/LEDs.h>
-		#include <LUFA/Drivers/Board/Buttons.h>
-		#include <LUFA/Drivers/Peripheral/Serial.h>
-		#include <LUFA/Drivers/Misc/RingBuffer.h>
-		#include <LUFA/Drivers/USB/USB.h>
-		#include <LUFA/Platform/Platform.h>
+#include <LUFA/Drivers/Board/LEDs.h>
+#include <LUFA/Drivers/Board/Buttons.h>
+#include <LUFA/Drivers/Peripheral/Serial.h>
+#include <LUFA/Drivers/Misc/RingBuffer.h>
+#include <LUFA/Drivers/USB/USB.h>
+#include <LUFA/Platform/Platform.h>
 
-	/* Macros: */
-		/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-		#define LEDMASK_USB_NOTREADY      LEDS_LED1
+/* Macros: */
+/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
+#define LEDMASK_USB_NOTREADY      LEDS_LED1
 
-		/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-		#define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
+/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
+#define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
 
-		/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-		#define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
+/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
+#define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
 
-		/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
-		#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
+/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
+#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
 
-	/* Function Prototypes: */
-		void SetupHardware(void);
-                void DecodeMouse(unsigned char* s, char* button, int* x, int* y);
+/* Function Prototypes: */
+void SetupHardware(void);
+void MySerial_Init(const uint32_t BaudRate);
+void DecodeMouse(unsigned char* s, char* button, int* x, int* y);
 
+void EVENT_USB_Device_Connect(void);
+void EVENT_USB_Device_Disconnect(void);
+void EVENT_USB_Device_ConfigurationChanged(void);
+void EVENT_USB_Device_ControlRequest(void);
+void EVENT_USB_Device_StartOfFrame(void);
 
-		void EVENT_USB_Device_Connect(void);
-		void EVENT_USB_Device_Disconnect(void);
-		void EVENT_USB_Device_ConfigurationChanged(void);
-		void EVENT_USB_Device_ControlRequest(void);
-		void EVENT_USB_Device_StartOfFrame(void);
+void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
 
-		void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
-
-		bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-		                                         uint8_t* const ReportID,
-		                                         const uint8_t ReportType,
-		                                         void* ReportData,
-		                                         uint16_t* const ReportSize);
-		void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
-		                                          const uint8_t ReportID,
-		                                          const uint8_t ReportType,
-		                                          const void* ReportData,
-		                                          const uint16_t ReportSize);
+bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
+        uint8_t* const ReportID,
+        const uint8_t ReportType,
+        void* ReportData,
+        uint16_t* const ReportSize);
+void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
+        const uint8_t ReportID,
+        const uint8_t ReportType,
+        const void* ReportData,
+        const uint16_t ReportSize);
 
 #endif
 
+// vim: noai:ts=4:sw=4
